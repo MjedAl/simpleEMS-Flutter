c81d60322c86a22178e1c11e88ba0d4c50086cf1
Abdulmajeed Al-ahmadi <44271342+MjedAl@users.noreply.github.com>
2021-08-09 03:32:19 +0300

    Testing GitGarduian
    Oh no I pushed something by mistake

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,5 @@
 class config {
   static final String API_LINK = "1/api/v1/";
+  static String privateKey = '-----BEGIN PRIVATE KEY-----\n3gxjaVEW6QWG9rCZUZ3FIwyQqte6Q6DJX3dR6mZfJA9==\n-----END PRIVATE KEY-----\n'
 }
+
